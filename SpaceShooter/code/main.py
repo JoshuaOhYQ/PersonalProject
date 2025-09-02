f43fba4e79661afc5f4f73e30040594bc3ffb282
JoshuaOhYQ <yuqiao.oh@gmail.com>
2025-09-03 01:49:00 +0800

    huidhuiddwa

--- SpaceShooter/code/main.py
+++ SpaceShooter/code/main.py
@@ -18,11 +18,15 @@ x = 100
 
 #Import images
 player_surf = pygame.image.load(join('SpaceShooter', 'images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (0,0))
-
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 star_surf = pygame.image.load(join('SpaceShooter', 'images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+meteor_surf = pygame.image.load(join('SpaceShooter', 'images', 'meteor.png')).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+
 
 while running:
     #Event loop
@@ -34,7 +38,13 @@ while running:
     display_surface.fill('dark blue')
     for pos in star_positions:
         display_surface.blit(star_surf, pos)
+
+    if player_rect.right < WINDOW_WIDTH:
+       player_rect.left += 0.2   
+    
+    
     display_surface.blit(player_surf, player_rect)
+    display_surface.blit(meteor_surf, meteor_rect)
     pygame.display.update()
 
 
